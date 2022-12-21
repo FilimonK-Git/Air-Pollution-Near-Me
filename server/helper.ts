0933bee5227e5ab48f340bcbec269848ae01ae25
Filimon Kiros <filimona.kiros@gmail.com>
2022-12-20 22:17:17 -0500

    ts conv in final stages

--- server/helper.ts
+++ server/helper.ts
@@ -1,7 +1,67 @@
 const axios = require("axios");
 const key = require("../client/config/config.js");
-const db = require("../database/database");
 const moment = require("moment");
+
+const airInfoGetter =(zipcode: string) =>{
+  const airGetOptions = {
+    method: "GET",
+    url: `https://api.ambeedata.com/latest/by-postal-code?postalCode=${zipcode}&countryCode=US`,
+    headers: {
+      "x-api-key": key.ambeeKey,
+      "Content-type": "application/json",
+    },
+  };
+
+  return axios
+    .request(airGetOptions)
+    .then((response) => {
+      if (response.data.stations[0]) {
+        var dataDate = moment.utc(response.data.stations[0].updatedAt).toDate();
+        var local = moment(dataDate).local().format("YYYY-MM-DD HH:mm:ss");
+        response.data.stations[0].updatedAt = local;
+        return response.data.stations[0];
+      } else {
+        return response.data;
+      }
+    })
+    .catch((err) => {
+      console.error("Error in Axios GET (see airInfoGetter fn())", err);
+    });
+};
+
+const worstAQIgetter =() =>{
+  const worstGetOptions = {
+    method: "GET",
+    url: "https://api.ambeedata.com/latest/by-order/worst",
+    headers: {
+      "x-api-key": key.ambeeKey,
+      "Content-type": "application/json",
+    },
+  };
+
+  return axios
+    .request(worstGetOptions)
+    .then((response) => {
+      var worstAQI = 0;
+      var worstPlace = undefined;
+
+      for (let i = 0; i < response.data.stations.length; i++) {
+        if (response.data.stations[i].AQI > worstAQI) {
+          worstAQI = response.data.stations[i].AQI;
+          worstPlace = response.data.stations[i];
+        }
+      }
+      return worstPlace;
+    })
+    .catch((err) => {
+      console.error("Error in Axios GET (see worstAQIgetter fn())", err);
+    });
+};
+module.exports = {airInfoGetter, worstAQIgetter}
+
+/*
+GRAPHQL
+
 // const {
 //   GraphQLSchema,
 //   GraphQLList,
@@ -30,68 +90,4 @@ const moment = require("moment");
 //     AQI: { type: GraphQLInt },
 //   }),
 // });
-
-const airInfoGetter = (zipcode) => {
-  // console.log(" hio");
-  const options = {
-    method: "GET",
-    url: `https://api.ambeedata.com/latest/by-postal-code?postalCode=${zipcode}&countryCode=US`,
-    headers: {
-      "x-api-key": key.ambeeKey,
-      "Content-type": "application/json",
-    },
-  };
-
-  return axios
-    .request(options)
-    .then((response) => {
-      if (response.data.stations[0]) {
-        // console.log("*****", response.data.stations);
-        var dataDate = moment.utc(response.data.stations[0].updatedAt).toDate();
-        var local = moment(dataDate).local().format("YYYY-MM-DD HH:mm:ss");
-
-        response.data.stations[0].updatedAt = local;
-        return response.data.stations[0];
-      } else {
-        return response.data;
-      }
-    })
-    .catch((err) => {
-      console.log("err in axios get req from api", err);
-      // return response;
-    });
-};
-
-const worstAQIgetter = () => {
-  const options2 = {
-    method: "GET",
-    url: "https://api.ambeedata.com/latest/by-order/worst",
-    //    https://api.ambeedata.com/latest/by-order/worst
-    headers: {
-      "x-api-key": key.ambeeKey,
-      "Content-type": "application/json",
-    },
-  };
-
-  return axios
-    .request(options2)
-    .then((response) => {
-      var worstAQI = 0;
-      var worstPlace;
-
-      for (let i = 0; i < response.data.stations.length; i++) {
-        if (response.data.stations[i].AQI > worstAQI) {
-          worstAQI = response.data.stations[i].AQI;
-          worstPlace = response.data.stations[i];
-        }
-      }
-
-      return worstPlace;
-    })
-    .catch((err) => {
-      console.log("err in axios worst get req from api", err);
-    });
-};
-
-exports.airInfoGetter = airInfoGetter;
-exports.worstAQIgetter = worstAQIgetter;
+*/
--- server/server.ts
+++ server/server.ts
@@ -2,22 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const api = require("../server/helper");
 const db = require("../database/database");
-const moment = require("moment");
-
+const moment2 = require("moment");
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static("client"));
 
-// ONE GET ROUTE REQUIRED
-
 app.post("/airq", (req, res) => {
   db.retrieve(req.body.zipSearch)
     .then((data) => {
-      // console.log(" HERE000", data);
       if (data[0]) {
-        var timeLapse = moment(
+        var timeLapse = moment2(
           data[0].updatedAt,
           "YYYY-MM-DD HH:mm:ss"
         ).fromNow();
@@ -31,10 +27,8 @@ app.post("/airq", (req, res) => {
           timeLapse.includes("day") ||
           timeLapse.includes("year")
         ) {
-          api
-            .airInfoGetter(req.body.zipSearch)
+          api.airInfoGetter(req.body.zipSearch)
             .then((data) => {
-              console.log("2nd get saved data in SERVER", data);
               db.secondSave(data)
                 .then((data) => {
                   var updatedDataToClient = {
@@ -51,32 +45,24 @@ app.post("/airq", (req, res) => {
                   };
                   res.send(updatedDataToClient);
                 })
-                .catch((err) => {
-                  console.log("data not updated in db from server", err);
+                .catch(() => {
                   res.status(400);
                 });
             })
-            .catch((err) => {
-              console.log("axios req failed in server", err);
+            .catch(() => {
               res.status(400);
             });
         } else {
           res.send(data[0]);
         }
       } else {
-        // console.log(" HERE", req.body.zipSearch);
-        api
-          .airInfoGetter(req.body.zipSearch)
+        api.airInfoGetter(req.body.zipSearch)
           .then((data) => {
-            console.log("data HERE", data instanceof Error);
-
             if (data instanceof Error) {
               res.send("Data not available!");
             } else {
               db.firstSave(data)
                 .then((data) => {
-                  console.log("--- ---", data);
-
                   var newDataToClient = {
                     placeName: data.placeName,
                     state: data.state,
@@ -91,36 +77,28 @@ app.post("/airq", (req, res) => {
                   };
                   res.send(newDataToClient);
                 })
-                .catch((err) => {
-                  console.log("data not saved in db from server", err);
+                .catch(() => {
                   res.status(400);
                 });
             }
           })
-          .catch((err) => {
-            console.log("axios req failed in server", err);
+          .catch(() => {
             res.status(400);
           });
       }
     })
-    .catch((err) => {
-      console.log("data not retrieved and not sent to client", err);
+    .catch(() => {
       res.status(400);
     });
 });
 
 app.get("/worst&best", (req, res) => {
-  console.log("get home fired");
 
-  // UNCOMMENT OUT WHEN READY TO CALL API FOR WORST CASES
-
-  Promise.all([api.worstAQIgetter()]) // , api.bestAQIgetter()
+  Promise.all([api.worstAQIgetter()])
     .then((data) => {
-      console.log("worst only", data);
       res.send(data);
     })
-    .catch((err) => {
-      console.log("axios worst/best req failed in server", err);
+    .catch(() => {
       res.status(400);
     });
 });
